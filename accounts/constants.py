b89934fa8cfd4aabd2a447278785291942b6f04b
sanjudha07 <117622802+sanjudha07@users.noreply.github.com>
2023-02-21 14:22:18 +0530

    Add files via upload

---
+++ accounts/constants.py
@@ -0,0 +1,15 @@
+MALE = 'M'
+FEMALE = 'F'
+
+GENDER_CHOICE = (
+    (MALE, "Male"),
+    (FEMALE, "Female"),
+)
+
+SAVINGS = 'S'
+CURRENT = 'C'
+
+ACCOUNT_TYPE = (
+    (SAVINGS, "Savings"),
+    (CURRENT, "Current"),
+)
